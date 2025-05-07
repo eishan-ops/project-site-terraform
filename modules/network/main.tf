45b76d4a59dde0167e6db8dfc587951cb5bbcea0
Eishan <eishan.rana@iqmetrix.com>
2025-05-07 12:26:06 -0700

    modularize network components

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,42 +1,41 @@
-# resource "aws_vpc" "ps_release_can_vpc_001" {
-#   cidr_block       = "10.0.0.0/22"
-#   instance_tenancy = "default"
+locals {
+
+  flat_subnets = flatten([ for vpc in var.vpc : [
+    for subnet in vpc.subnets : {
+      name = subnet.name
+      cidr_block = subnet.cidr_block
+      vpc_id = aws_vpc.main[vpc.name].id
+    }
+  ] 
+ ])
+
+}
+
+output "subnets" {
+  value = local.flat_subnets
+}
+
+resource "aws_vpc" "main" {
+  for_each = { for vpc in var.vpc : vpc.name => vpc }
+  cidr_block       = each.value.cidr_block
+  instance_tenancy = "default"
   
-#   tags = {
-#     Name = "ps-release-can-vpc-001"
-#     Managed-By = "terraform"
-#     Env = "release"
-#   }
-# }
+  tags = {
+    Name = each.key
+    Managed-By = "terraform"
+    Env = "release"
+  }
+}
 
-# resource "aws_subnet" "ps_release_can_subnet" {
-#   vpc_id     = aws_vpc.ps_release_can_vpc_001.id
-#   cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "main" {
+  for_each   = { for subnet in local.flat_subnets : subnet.name => subnet }
+  vpc_id     = each.value.vpc_id
+  cidr_block = each.value.cidr_block
 
-#   tags = {
-#     Managed-By = "terraform"
-#     Env = "release"
-#   }
-# }
-
-
-# for this to work we will need 
-# vpc {
-#     snet1 = {
-
-#     }
-
-#     snet2 = {
-
-#     }
-# }
-
-#naming convention 
-
-# project-env-region-resource--purpose,number
-
-# Examples 
-
-# project-site-release-canadacentral-vpc
-# project-site-release-canadacentral-ec2-web001
+  tags = {
+    Name = each.key
+    Managed-By = "terraform"
+    Env = "release"
+  }
+}
 
